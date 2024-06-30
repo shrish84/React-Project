bc3539344435cee6bc7b0765e09418af1afcb7b7
Akshay Garg <Agarg@hollandamerica.com>
2024-06-30 13:21:57 +0530

    basic structure

---
+++ Todo-Project/src/component/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectsSidebar = () => {
+  return (
+    <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
+        <h1>YOUR PROJECTS</h1>
+        <div>
+            <button>+Add Project</button>
+        </div>
+     <ul></ul> 
+    </aside>
+  )
+}
+
+export default ProjectsSidebar
